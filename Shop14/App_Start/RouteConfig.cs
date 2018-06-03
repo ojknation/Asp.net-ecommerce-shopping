c15d6535f5968c3db616fa33dd1226459a08adf3
ojknation <ojknation@gmail.com>
2018-06-03 21:48:56 +0100

    pages and other minor touches done

--- Shop14/App_Start/RouteConfig.cs
+++ Shop14/App_Start/RouteConfig.cs
@@ -13,11 +13,19 @@ namespace Shop14
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Pages", "{Page}", new { controller = "Pages", action = "Index" }, new[] { "Shop14.Controllers" });
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "Shop14.Controllers" });
+            routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] { "Shop14.Controllers" });
+            routes.MapRoute("SidebarPartial", "Pages/SidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "Shop14.Controllers" });
+
+
+
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional } 
+            //);
         }
     }
 }
---
+++ Shop14/Controllers/PagesController.cs
@@ -0,0 +1,86 @@
+﻿using Shop14.Models.Data;
+using Shop14.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop14.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Pages
+        public ActionResult Index(string page = "")
+        {
+            //Get/set page slut
+            if (page == "")
+                page = "home";
+            //Declare model and DTO
+            PageVM model;
+            PageDTO dto;
+
+            //Check if page exists
+            using (Db db = new Db())
+            {
+                if (! db.Pages.Any(x => x.Slug.Equals(page)))
+                {
+                    return RedirectToAction("Index", new { page = "" });
+                }
+            }
+            //Get Page DTO
+            using (Db db = new Db())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+
+            //Set page title 
+            ViewBag.PageTitle = dto.Title;
+
+            //Check for sidebar
+            if (dto.HasSidebar == true)
+            {
+                ViewBag.Sidebar = "Yes";
+            }
+            else
+            {
+                ViewBag.Sidebar = "No";
+            }
+
+            //init model
+            model = new PageVM(dto);
+
+                return View(model);
+        }
+
+        public ActionResult PagesMenuPartial()
+        {
+            //Declare a list of PageVM
+            List<PageVM> pageVMList;
+
+            //Get all pages except home
+            using (Db db = new Db())
+            {
+                pageVMList = db.Pages.ToArray().OrderBy(x => x.Sorting).Where(x => x.Slug != "home").Select(x => new PageVM(x)).ToList();
+            }
+
+            //Return the partial view with list
+                return PartialView(pageVMList);
+        }
+
+        public ActionResult SidebarPartial()
+        {
+            //Declare model
+            SidebarVM model;
+
+            //Initialize model
+            using (Db db = new Db())
+            {
+                SidebarDTO dto = db.Sidebar.Find(1);
+                model = new SidebarVM(dto);
+            }
+                //Return partial view with model
+                return PartialView(model);
+        }
+    }
+}
